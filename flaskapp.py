5f957b799f592d061fbad7dbdc173865a942d84a
Matthew Goldsberry <mattheweg99@gmail.com>
2026-02-10 16:48:16 -0500

    add login validation and handling

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,13 @@
 """Flask app implementation."""
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
+from werkzeug import Response
 
-from helpers import db_write_query
+from environment import FLASK_DEBUG, FLASK_SECRET_KEY
+from helpers import db_read_query, db_write_query
 
 app = Flask(__name__)
+app.secret_key = FLASK_SECRET_KEY
 
 # sqlite setup
 db_write_query(
@@ -21,14 +24,34 @@ db_write_query(
 
 
 @app.route("/")
-def index():
+def index() -> str:
+    """Landing Page for the Flask App.
+
+    Returns:
+        Rendering of the login.html file
+    """
     return render_template("login.html")
 
 
 @app.route("/login", methods=["POST"])
-def login():
+def login() -> Response | str:
+    """Try to log in user with provided credentials, raising an error if invalid.
+
+    Returns:
+        Rendering of profile page if correct credentials, else redirect to login page with error message
+    """
     username = request.form.get("username")
     password = request.form.get("password")
+
+    db_username, db_password, _, _, _ = db_read_query(
+        "select * from users where username = ?",
+        params=(username,),
+    )
+
+    if not db_username or db_password != password:
+        flash("Invalid username or password.", "error")
+        return redirect(url_for("index"))
+
     return render_template("profile.html")
 
 
@@ -59,4 +82,4 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(FLASK_DEBUG)
